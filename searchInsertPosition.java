c9d6fe79b4c865f36128a067be1e6ed4ad60bd2f
alexma3109 <mayixin3109@gmail.com>
2015-07-04 23:45:45 -0700

    <= start, <= end, else

---
+++ searchInsertPosition.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length == 0){
+			return -1;
+		}
+		int start = 0; 
+		int end = nums.length - 1;
+		int mid = 0;
+		while(start + 1 < end){
+			mid = start + (end - start) / 2;
+			if (nums[mid] == target){
+				return mid;
+			}
+			else if (nums[mid] < target){
+				start = mid;
+			}
+			else {
+				end = mid;
+			}			
+		}
+		if (target <= nums[start]){
+			return start;
+		}
+		else if (target <= nums[end]){
+			return end;
+		}
+		else{
+			return end + 1;
+		}
+    }
+}
