b7abb8d0784895935d2d7fb3700e504f6b28b778
alexma3109 <mayixin3109@gmail.com>
2015-07-06 01:05:18 -0700

    when not recersive, push right in stack first, not left

---
+++ binaryTreePreorder.java
@@ -0,0 +1,44 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Solution {
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+		helper(root, res);
+		return res;
+    }
+	public void helper(TreeNode root, ArrayList<Integer> res){
+		if(root == null){
+			return;
+		}
+		res.add(root.val);
+		helper(root.left, res);
+		helper(root.right, res);
+	}
+}
+
+public class Solution {
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		if(root == null){
+			return res;
+		}
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		stack.push(root);
+		while(!stack.isEmpty()){
+			TreeNode temp = stack.pop();
+			res.add(temp.val);
+			if(temp.right != null){
+				stack.push(temp.right);
+			}
+			if(temp.left != null){
+				stack.push(temp.left);
+			}
+		}
+		return res;
+	}
+}
