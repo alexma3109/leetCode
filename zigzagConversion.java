980d8e216850a94e29fc6dc72f25916cd2a3fad2
alexma3109 <mayixin3109@gmail.com>
2015-07-25 22:38:32 -0700

    size = 2n-2

---
+++ zigzagConversion.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1){
+			return s;
+		}
+		int size = 2 * numRows - 2;
+		stringBuilder sb = new stringBuilder();
+		for(int i = 0 ; i < numRows ; i++){
+			for(int j = i ; j < s.length() ; j += size){
+				sb.append(s.charAt(j));
+				if(i != 0 && i != numRows - 1 && j + size - 2 * i < s.length()){
+					sb.append(s.charAt(j + size - 2 * i));
+				}
+			}
+		}
+		return sb.toString();
+    }
+}
