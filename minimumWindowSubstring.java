bc15aaf4a6fc768392961d92db1dbbb71496968f
alexma3109 <mayixin3109@gmail.com>
2015-08-01 00:34:05 -0700

    updated

--- minimumWindowSubstring.java
+++ minimumWindowSubstring.java
@@ -1,6 +1,6 @@
 public class Solution {
     public String minWindow(String s, String t) {
-        HashMap<character, Integer> map = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 		for(int i = 0 ; i < t.length() ; i++){
 			if(map.containsKey(t.charAt(i))){
 				map.put(t.charAt(i), map.get(t.charAt(i)) + 1);
@@ -9,25 +9,24 @@ public class Solution {
 				map.put(t.charAt(i), 1);
 			}
 		}
-		int minLen = s.length() + 1;
-		int left = 0;
+		int count = 0;
+		int minLen = s.length();
 		int minStart = 0;
-		int count = t.length();
-		for(i < 0 ; i < s.length() ; i++){
+		int left = 0;
+		for(int i = 0 ; i < s.length() ; i++){
 			if(map.containsKey(s.charAt(i))){
 				map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
-				if(map.get(s.charAt(i) >= 0)){
+				if(map.get(s.charAt(i)) >= 0){
 					count++;
 				}
-				while(count == t.length()){
+				while(count == s.length()){
 					int len = i - left + 1;
 					if(len < minLen){
 						minLen = len;
-						minStart = left;
 					}
 					if(map.containsKey(s.charAt(left))){
 						map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
-						if(map.get(s.charAt(left) > 0)){
+						if(map.get(s.charAt(left)) > 0){
 							count--;
 						}
 					}
