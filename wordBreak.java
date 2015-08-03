23afd95badc79814723bd8556aa04e2932b40846
alexma3109 <mayixin3109@gmail.com>
2015-08-02 23:04:45 -0700

    updated

--- wordBreak.java
+++ wordBreak.java
@@ -2,7 +2,7 @@ public class Solution {
     public boolean wordBreak(String s, Set<String> wordDict) {
         boolean[] res = new boolean[s.length() + 1];
 		res[0] = true;
-		for(int i = 1 ; i <= res.length ; i++){
+		for(int i = 1 ; i < res.length ; i++){
 			for(int j = 0 ; j < i ; j++){
 				if(res[j] && wordDict.contains(s.substring(j, i))){
 					res[i] = true;
