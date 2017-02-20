c65b05a33fd4b5d07b70c68a0eaa5def75bfee4e
alexma3109 <myx@Yixins-MacBook-Pro.local>
2017-02-19 19:28:42 -0800

    init

---
+++ minimumDepthOfBinaryTree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int depth = 0;
+        while(!q.isEmpty()){
+            depth++;
+            int len = q.size();
+            for(int i = 0 ; i < len ; i++){
+                TreeNode node = q.poll();
+                if(node.left == null && node.right == null){
+                    return depth;
+                }
+                if(node.left != null){
+                    q.offer(node.left);
+                }
+                if(node.right != null){
+                    q.offer(node.right);
+                }
+            }
+        }
+        return depth;
+    }
+}
