83a3fbdf00f5c834a9da8e0583a941239ab54903
alexma3109 <myx@Yixins-MacBook-Pro.local>
2017-06-21 21:42:32 -0700

    init&upadte

--- validNumber.java
+++ validNumber.java
@@ -49,3 +49,41 @@ public class Solution {
 		return num;
 	}
 }
+/////////////////////////////
+public class Solution {
+    public boolean isNumber(String s) {
+        s = s.trim();
+        boolean num = false, exp = false, dot = false;
+        for(int i = 0 ; i < s.length() ; i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '+':
+                case '-':
+                    if(i != 0 && s.charAt(i - 1) != 'e') {
+                        return false;
+                    }
+                    num = false;
+                    break;
+                case '.': // '.' can be the last char or followed by 'e'
+                    if(exp || dot) {
+                        return false;
+                    }
+                    dot = true;
+                    break;
+                case 'e':
+                    if(exp || !num) { // previous char has to be num
+                        return false;
+                    }
+                    num = false; // 'e' cannot be the last char
+                    exp = true;
+                    break;
+                default:
+                    num = Character.isDigit(c);
+                    if(!num) {
+                        return false;
+                    }
+            }
+        }
+        return num;
+    }
+}
